b69d6fd2404ac1bfdb737d5cacbdad6cf5efbbde
Indu Ramesh <induramesh@gmail.com>
2019-07-31 01:30:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,4 +11,10 @@ def sort_array_char_count(arr)
   sorted_arr = arr.sort_by {|s| s.length}
   sorted_arr 
 end 
+
+def swap_elements(arr)
+  temp = arr[1]
+  arr[1] = arr[2]
+  arr[2] = temp 
+end 
   
