e8409b5f7962307df8e881ccb8a32eb1009d72d6
Indu Ramesh <induramesh@gmail.com>
2019-07-31 01:29:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,4 +3,12 @@ def sort_array_asc(arr)
 end 
 
 def sort_array_desc(arr)
+  sorted_arr = arr.sort_by { |number| -number }
+  sorted_arr
 end 
+
+def sort_array_char_count(arr)
+  sorted_arr = arr.sort_by {|s| s.length}
+  sorted_arr 
+end 
+  
